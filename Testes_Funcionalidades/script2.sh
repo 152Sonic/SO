c3effdf2e86a82af46189cabef8644af7e80a316
ZeCosta <josepedroc.costa@gmail.com>
2020-06-29 17:29:26 +0100

    scripts de teste iniciais

---
+++ Testes_Funcionalidades/script2.sh
@@ -0,0 +1,16 @@
+./argus -t 2
+./argus -e "ls -l | wc "
+./argus -e "ls -l |wc"
+./argus -t 2
+./argus -t 2
+./argus -t 2
+./argus -i 10
+./argus -m 2
+./argus -e "ls -l | wc "
+./argus -e "ls -l |wc"
+./argus -e "ls -l | wc "
+./argus -i 12
+./argus -e "date"
+./argus -m 24 
+./argus -t 2
+./argus -e "ls -l | wc "
